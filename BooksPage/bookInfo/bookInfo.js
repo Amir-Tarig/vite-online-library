37c699d36090dca44fb5535d9a563b822a4488be
Miro <MiroPhoenix91@gmail.com>
2021-09-21 19:27:20 +0200

    first commit

---
+++ BooksPage/bookInfo/bookInfo.js
@@ -0,0 +1,52 @@
+
+function fetchingBooks() {
+    let newBookList = []
+    const Book = {
+        title: null,
+        categories: null,
+        author: null,
+        description: null,
+        image: null,
+        publisher: null,
+        publishedDate: null,
+        id: null,
+        price: 0
+    }
+
+    fetch("https://google-books.p.rapidapi.com/volumes?key=AIzaSyAOsteuaW5ifVvA_RkLXh0mYs6GLAD6ykc", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "google-books.p.rapidapi.com",
+            "x-rapidapi-key": "7ecbb014b0mshc95dbe51b85f5d9p186b56jsn682d5e35fb85"
+        }
+    })
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        newBookList = [...data.items]
+        newBookList.map((book, i) => {
+            let fixedSaleability =  book.saleInfo.saleability.replace(/_/g, ' ')
+
+                if(!book.volumeInfo.description) book.volumeInfo.description = 'This book has no discription'
+                if(!book.volumeInfo.categories) book.volumeInfo.categories = 'unknown'
+                if(!book.volumeInfo.publisher) book.volumeInfo.publisher = 'Unknown publisher'
+                !book.saleInfo.listPrice ? Book.price = fixedSaleability 
+                : book.saleInfo.listPrice.amount === 0 ? Book.price = "FREE" 
+                : Book.price =  `${book.saleInfo.listPrice.amount} ${book.saleInfo.listPrice.currencyCode}`;
+
+                Book.title = book.volumeInfo.title
+                Book.categories = book.volumeInfo.categories[0] 
+                Book.author = book.volumeInfo.authors[0]
+                Book.description = book.volumeInfo.description
+                Book.image = book.volumeInfo.imageLinks.smallThumbnail
+                Book.publisher = book.volumeInfo.publisher
+                Book.publishedDate = book.volumeInfo.publishedDate
+                Book.id = book.id
+                displayBook(Book)
+            })
+    })
+    .catch(err => {
+        console.error(err);
+    });
+}
---
+++ BooksPage/books.js
@@ -0,0 +1,106 @@
+function fetchingBooks() {
+    let newBookList = []
+    const Book = {
+        title: null,
+        categories: null,
+        author: null,
+        description: null,
+        image: null,
+        publisher: null,
+        publishedDate: null,
+        id: null,
+        price: 0
+    }
+
+    fetch("https://google-books.p.rapidapi.com/volumes?key=AIzaSyAOsteuaW5ifVvA_RkLXh0mYs6GLAD6ykc", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "google-books.p.rapidapi.com",
+            "x-rapidapi-key": "7ecbb014b0mshc95dbe51b85f5d9p186b56jsn682d5e35fb85"
+        }
+    })
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        newBookList = [...data.items]
+        newBookList.map((book, i) => {
+            let fixedSaleability =  book.saleInfo.saleability.replace(/_/g, ' ')
+
+                if(!book.volumeInfo.description) book.volumeInfo.description = 'This book has no discription'
+                if(!book.volumeInfo.categories) book.volumeInfo.categories = 'unknown'
+                if(!book.volumeInfo.publisher) book.volumeInfo.publisher = 'Unknown publisher'
+                !book.saleInfo.listPrice ? Book.price = fixedSaleability 
+                : book.saleInfo.listPrice.amount === 0 ? Book.price = "FREE" 
+                : Book.price =  `${book.saleInfo.listPrice.amount} ${book.saleInfo.listPrice.currencyCode}`;
+
+                Book.title = book.volumeInfo.title
+                Book.categories = book.volumeInfo.categories[0] 
+                Book.author = book.volumeInfo.authors[0]
+                Book.description = book.volumeInfo.description
+                Book.image = book.volumeInfo.imageLinks.smallThumbnail
+                Book.publisher = book.volumeInfo.publisher
+                Book.publishedDate = book.volumeInfo.publishedDate
+                Book.id = book.id
+                displayBook(Book)
+            })
+    })
+    .catch(err => {
+        console.error(err);
+    });
+}
+
+function displayBook(book) {
+    const booksContainer = document.querySelector('.booksContainer')
+
+    let imgPriceContainer = document.createElement('div')
+    let buttonsContainer = document.createElement('div')
+    let Book = document.createElement('div')
+
+    let imgTag = document.createElement('img')
+    let priceTag = document.createElement('p')
+
+    let btn1 = document.createElement('a')
+    let btn2 = document.createElement('button')
+
+    btn1.classList.add('link')
+    btn2.classList.add('btn')
+
+    btn1.href = "/BooksPage/bookInfo/bookInfo.html"
+    btn1.textContent = "INFO"
+    btn2.innerText = "ADD"
+
+
+       imgTag.classList.add('bookCover')
+       priceTag.classList.add('price')
+       Book.classList.add('book')
+
+       imgPriceContainer.classList.add('img-price')
+       buttonsContainer.classList.add('btnContainer')
+
+      
+       
+
+       imgTag.src = book.image
+       priceTag.innerText = book.price
+
+      
+
+    
+       imgPriceContainer.appendChild(imgTag)
+       imgPriceContainer.appendChild(priceTag)
+
+       buttonsContainer.appendChild(btn1)
+       buttonsContainer.appendChild(btn2)
+
+       Book.appendChild(imgPriceContainer)
+       Book.appendChild(buttonsContainer)
+
+       booksContainer.appendChild(Book)
+    // console.log(books)
+}
+
+
+fetchingBooks() 
+
+
